9ab002cddf2a3a55a5387ee792735287c69cca50
bronemos <spieglb@gmail.com>
2022-01-05 17:47:55 +0100

    L3 bomb-baby

---
+++ L3/bomb-baby/solution.py
@@ -0,0 +1,24 @@
+
+
+def solution(x, y):
+    x = int(x)
+    y = int(y)
+    count = 0
+    while x != 1 and y != 1:
+        if x <= 0 or y <= 0:
+            return "impossible"
+        elif y > x:
+            count += y // x
+            y = y - x * (y // x)
+        elif x > y:
+            count += x // y
+            x = x - y * (x // y)
+    if x == 1:
+        count += y
+    elif y == 1:
+        count += x
+    return str(count - 1)
+
+
+assert solution("4", "7") == "4"
+assert solution("2", "1") == "1"
